f4b4dae8644d620afa565b718e465f4dfe3f5624
Joseph Kain <joekain@gmail.com>
2015-10-19 23:08:23 -0700

    Move all completions to 01completion.sh

---
+++ 01completion.sh
@@ -0,0 +1,4 @@
+. /usr/local/etc/bash_completion.d/mix
+. /usr/local/etc/bash_completion.d/git-completion.bash
+. /usr/local/etc/bash_completion.d/cargo
+. /usr/local/etc/bash_completion.d/ag.bashcomp.sh
