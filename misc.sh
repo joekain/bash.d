b31662559756a1053d6c2af3f2c32de15e93ef44
Joseph Kain <joekain@gmail.com>
2016-02-19 09:53:16 -0800

    Setup iTerm2 integration

--- misc.sh
+++ misc.sh
@@ -1,3 +1,7 @@
+
 PATH=$PATH:~/tools/phantomjs-1.9.2-macosx/bin
 PATH=$PATH:~/.golang/go1.2.2/bin
 PATH=$PATH:~/.scala/scala-2.11.1/bin/
+
+# iTerm2 shell integration
+test -e ${HOME}/.iterm2_shell_integration.bash && source ${HOME}/.iterm2_shell_integration.bash
