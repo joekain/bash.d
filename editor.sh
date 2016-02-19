ba5eaa27849dd341fbd073a765c2e9b653b213ab
Joseph Kain <joekain@gmail.com>
2016-02-19 09:52:02 -0800

    Configure Atom as EDITOR

---
+++ editor.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export EDITOR=atom
+export GIT_EDITOR=giteditor
+
+alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs'
+
+export PATH=/Applications/Emacs.app/Contents/MacOS/bin:"$PATH"
